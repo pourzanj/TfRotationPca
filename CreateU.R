af183263ab06cfa144fea365bab4a90f511d8242
pourzanj <arya@umail.ucsb.edu>
2017-03-29 12:58:31 -0700

    updated program layout to include details on how to sample angles on an unconstrained space. Also fixedx CreateU to reflect that now we pass in the angles as a rank 1 tensor instead of a list of rank 0 tensors.

--- CreateU.R
+++ CreateU.R
@@ -2,9 +2,10 @@ library(tensorflow)
 
 sess <- tf$Session()
 
-Theta01 <- tf$placeholder(tf$float32)
-Theta02 <- tf$placeholder(tf$float32)
-Theta12 <- tf$placeholder(tf$float32)
+Theta01 <- tf$placeholder(tf$float32, shape = shape())
+Theta02 <- tf$placeholder(tf$float32, shape = shape())
+Theta12 <- tf$placeholder(tf$float32, shape = shape())
+Theta <- tf$stack(list(Theta01, Theta02, Theta12))
 
 LambdaVec <- tf$placeholder(tf$float32, shape(2))
 Lambda <- tf$diag(LambdaVec)
@@ -13,15 +14,15 @@ LambdaSq <- Lambda^2
 SigmaSq <- tf$placeholder(tf$float32)
 Id <- tf$constant(diag(3),dtype = tf$float32)
 
-G <- CreateGivensMatrix(list(Theta01, Theta02, Theta12), n = 3, p = 2)
+G <- CreateGivensMatrix(Theta, n = 3, p = 2)
 W <- G[,0:1]
 
 C <- tf$matmul(W, tf$matmul(LambdaSq, tf$transpose(W))) + SigmaSq*Id
 SigmaHat <- tf$constant(SigmaHat_, dtype = tf$float32)
-Area <- GetStiefelAreaForm(G, list(Theta01, Theta02, Theta12), n = 3, p = 2)
+Area <- GetStiefelAreaForm(G, Theta, n = 3, p = 2)
 
-U <- (10000/2)*log(tf$matrix_determinant(C)) + (10000/2)*tf$trace(tf$matrix_solve(C, SigmaHat)) - log(Area)
+U <- (1000/2)*log(tf$matrix_determinant(C)) + (1000/2)*tf$trace(tf$matrix_solve(C, SigmaHat)) - log(Area)
 GradU <- tf$gradients(U, list(Theta01, Theta02, Theta12, LambdaVec))
 
-system.time(for(i in 1:1000)sess$run(GradU, feed_dict = dict(Theta01 = pi/4, Theta02 = pi/4, Theta12 = 0, LambdaVec = list(5,3), SigmaSq = 1)))
+#system.time(for(i in 1:1000)sess$run(GradU, feed_dict = dict(Theta01 = pi/4, Theta02 = pi/4, Theta12 = 0, LambdaVec = list(5,3), SigmaSq = 1)))
 
